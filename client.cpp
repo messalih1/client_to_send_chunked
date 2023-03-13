d407bf4c8d2dcdd409a39050cade03cd05a7263c
mustapha essalih <86233431+messalih1@users.noreply.github.com>
2023-03-13 23:13:44 +0100

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,100 +1,26 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <arpa/inet.h>
- #include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
-#include <iconv.h>
-#include <iostream>
-#include <iostream>
-#include <algorithm>
-#include <utility>
-#include <memory>
-#include <stack>
-#include <vector>
-#include <map>
- #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <iterator>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <cstring>
-#include <sys/types.h>
-#include <sys/event.h>
-#include <sys/time.h>
-#include <exception>
-#include <type_traits>
- 
+import random
+import os
+import sys
+import requests
 
-using std::string;
-using std::cout;
-using std::endl;
-using std::vector;
-using std::stack;
+# Open the file and read its contents
+filename = 'test\ copy.txt'
+with open(filename, 'rb') as f:
+    data = f.read()
 
+# Define the endpoint URL and headers
+url = '127.0.0.1:8085'
+headers = {'Transfer-Encoding': 'chunked'}
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#define PORT 8080
+# Send the data in chunks
+while len(data) > 0:
+    # Choose a random chunk size between 1 and 10 bytes
+    chunk_size = random.randint(5055, 10000)
 
-int main(int argc, char const *argv[])
-{
-  std::string filename = "file";
-  std::ifstream file(filename);
+    # Send the next chunk
+    chunk = data[:chunk_size]
+    data = data[chunk_size:]
+    requests.post(url, headers=headers, data='%x\r\n%s\r\n' % (chunk_size, chunk))
 
-  if (!file) {
-    std::cerr << "Failed to open file: " << filename << std::endl;
-    return 1;
-  }
-
-  std::string line;
-  std::getline(file, line);
-  
-  // while (std::getline(file, line)) {
-  //   std::cout << line << std::endl;
-  // }
-    int sock = 0; long valread;
-    struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-    
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-    
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    send(sock , (void*)(line.data()) , line.size() , 0 );
-    
-    return 0;
-}
+# Send the final chunk with size 0
+requests.post(url, headers=headers, data='0\r\n\r\n')
